b9acb0e6590627e346382ba530430b017a820563
Nate Gay <nate@ohnate.com>
2017-03-07 02:35:31 -0600

    new tag colors and fixed tags extending beyond viewport

--- src/shared/Tags.js
+++ src/shared/Tags.js
@@ -3,23 +3,26 @@ import { Label } from 'react-bootstrap'
 import './tags.css'
 
 const tagColor = {
-  'React': 'danger',
-  'Redux': 'warning',
-  'Bootstrap': 'info',
-  'Wunderground API': 'primary',
-  'Node.js': 'success',
-  'Express': 'info',
-  'PHP': 'warning',
-  'WordPress API': 'info',
-  'Chrome API': 'success',
-  'Firebase': 'danger',
-  'jQuery': 'primary'
+  'React': 'label-react',
+  'Redux': 'label-redux',
+  'Bootstrap': 'label-bootstrap',
+  'Wunderground API': 'label-primary',
+  'Node.js': 'label-nodejs',
+  'Express': 'label-express',
+  'PHP': 'label-php',
+  'WordPress API': 'label-wordpress',
+  'Chrome API': 'label-success',
+  'Firebase': 'label-firebase',
+  'jQuery': 'label-jquery',
+  'GraphQL': 'label-graphql',
+  'Docker': 'label-docker',
+  'Kubernetes': 'label-kubernetes'
 }
 
 const Tags = (props) => (
   <span className='tags'>
   {props.tags.map((tag, index) => (
-    <Label key={index} bsStyle={tagColor[tag]} bsSize='large'>{tag}</Label>
+    <Label key={index} className={tagColor[tag]} bsSize='large'>{tag}</Label>
   ))}
   </span>
 )
